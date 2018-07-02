dcd79cb6bdc7369afd5e6e0e945ffd9666eafafc
Mosaab Muhammed <mosaabmuhammed@outlook.com>
2018-07-02 12:10:09 +0200

    Initial Commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,10 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Item::class, function (Faker $faker) {
+    return [
+        'text' => $faker->sentence,
+        'body' => $faker->paragraph,
+    ];
+});
