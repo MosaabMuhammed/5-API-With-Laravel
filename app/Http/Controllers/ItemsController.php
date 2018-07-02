0cf2b9a4b44f0cf19debbd1cb0ea70240e0e0335
Mosaab Muhammed <mosaabmuhammed@outlook.com>
2018-07-02 17:39:59 +0200

    Initial Commit

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -15,7 +15,7 @@ class ItemsController extends Controller
      */
     public function index()
     {
-        $items = Item::all();
+        $items = Item::latest()->get();
         return response()->json($items);
     }
 
